409abafd44a9d5e19f130058d069317a31719876
Mona Naamani <134978312+MonaNaamani@users.noreply.github.com>
2023-08-20 05:09:22 +0300

    Add code for dropdown

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
-// This function creates divs based on a chosen number, and it appends them to the DOM + it decides the height and width attributes.
+// Dropdown button
+
+function createOptions () {
+  const select = document.querySelector('.select');
+  const selectElements = [];
+
+  for (let i = 0; i < 64; i++) {
+    selectElements[i] = document.createElement('option');
+    selectElements[i].classList.add(`${(i + 1)}`);
+    selectElements[i].textContent = `${i + 1} x ${i + 1}`;
+    select.appendChild(selectElements[i]);
+  }
+  return selectElements;
+}
+
+createOptions();
+
+// Create an eventlistener for all Options that grabs the class of the option element
+
+let optionChoice = document.getElementById("size");
+
+optionChoice.addEventListener("click", getClassName);
+
+function getClassName () {
+  let collection = optionChoice.selectedOptions;
+  let output = "";
+  for (let i = 0; i < collection.length; i++) {
+    output = collection[i].className;
+  }
+    removeDivs();
+    createDivs(output);
+  };
+
+// This function creates divs based on a chosen number, and it appends them to the DOM + it decides the height and width attributes + color them
+
 function createDivs (a) {
   
   const container = document.querySelector('.container');
@@ -12,26 +46,15 @@ function createDivs (a) {
     divs[i].setAttribute('style', `height: ${height -1*2}px; width: ${width -1*2}px;`);
     container.appendChild(divs[i]);
   }
-  return divs;
+  divs.forEach(newDiv => newDiv.addEventListener('mouseover', mouseOver))
 }
 
+createDivs(1);
 // Hover event - it adds a class
-
-const allNewDivs = createDivs(5);
-
-allNewDivs.forEach(newDiv => newDiv.addEventListener('mouseover', mouseOver))
 
 function mouseOver(e) {
   this.classList.add('hovered');
   this.style.backgroundColor = `${randColor()}`;
-}
-
-// Hover event - it removes a class
-
-allNewDivs.forEach(newDiv => newDiv.addEventListener('mouseout', mouseOut))
-
-function mouseOut(e) {
-  this.classList.remove('hovered');
 }
 
 // event that colors the divs randomly
@@ -40,19 +63,10 @@ function randColor () {
      return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 }
 
-// Dropdown button
-
-function createOptions () {
-  const select = document.querySelector('.select');
-  const selectElements = [];
-
-  for (let i = 0; i < 64; i++) {
-    selectElements[i] = document.createElement('option');
-    selectElements[i].classList.add(`option${(i + 1)}`);
-    selectElements[i].textContent = `${i + 1} x ${i + 1}`;
-    select.appendChild(selectElements[i]);
+function removeDivs () {
+  const container = document.querySelector('.container');
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
   }
-  return selectElements;
 }
 
-createOptions();
